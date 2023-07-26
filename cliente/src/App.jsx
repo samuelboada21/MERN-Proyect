b772bacc10b6b327fe4dda716d8e9336379e468a
samuelboada21 <samuelomarboba@ufps.edu.co>
2023-07-26 16:34:20 -0500

    Subiendo Archivos, registro completado

---
+++ cliente/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/tasks" element={<h1>Tasks page</h1>} />
+        <Route path="/add-task" element={<h1>new task</h1>} />
+        <Route path="/tasks/:id" element={<h1>update task</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+      </Routes>
+    </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
