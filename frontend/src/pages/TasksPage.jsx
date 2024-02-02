2ae5f4d15faaabf4f347d63fe2ea178d14b30608
samuelboada21 <samuelomarboba@ufps.edu.co>
2024-02-02 15:22:40 -0500

    asincronico

--- frontend/src/pages/TasksPage.jsx
+++ frontend/src/pages/TasksPage.jsx
@@ -1,26 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useTasks } from "../context/TasksContext";
 import TaskCard from "../components/TaskCard";
 
 function TasksPage() {
-
-  const { getTasks, tasks } = useTasks();
+  const { getTasks } = useTasks();
+  const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    getTasks();
-  },[getTasks])
+    // Hacemos la función getTasks asíncrona
+    const fetchData = async () => {
+      try {
+        const tasksData = await getTasks();
+        setTasks(tasksData);
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+      }
+    };
 
-  if(tasks.length === 0) return (<h1>No tasks</h1>);
+    fetchData();
+  }, [getTasks]);
+
+  if (tasks.length === 0) return <h1>No tasks</h1>;
 
   return (
     <div className="sm:grid sm:grid-cols-2 md:grid-cols-3 gap-4 mb-32 mt-8">
-      {
-        tasks.map(task =>(
-          <TaskCard task={task} key={task._id}/>
-        ))
-      }
+      {tasks.map((task) => (
+        <TaskCard task={task} key={task._id} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default TasksPage
+export default TasksPage;
