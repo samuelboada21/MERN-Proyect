d975d31cc7d2c83a45325160ee4aa19643854685
samuelboada21 <samuelomarboba@ufps.edu.co>
2024-02-06 16:25:57 -0500

    token

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -55,7 +55,7 @@ export const login = async (req, res) => {
   
       // res.cookie("token", token);
       res.cookie("token", token, {
-        httpOnly: false,
+        httpOnly: true,
         sameSite: "None",
         secure: process.env.NODE_ENV === "production",
       });
